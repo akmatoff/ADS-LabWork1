05018631f98fb0a383198cd72f1bc9917f9a441e
akmatoff <azim.akmatov1@gmail.com>
2020-01-19 22:08:29 +0600

    Started learning linked list

--- src/BooksList.java
+++ src/BooksList.java
@@ -1,2 +1,39 @@
-public class BooksList {
+import java.awt.print.Book;
+
+class BooksList {
+    private BookListElement head;
+    private BookListElement tail;
+
+    void addFront(String udk) {
+        BookListElement a = new BookListElement();
+        a.udk = udk;
+
+        if(head == null) {
+            head = a;
+            tail = a;
+        } else {
+            a.next = head;
+            head = a;
+        }
+    }
+
+    void addBack(String udk) {
+        BookListElement a = new BookListElement();
+        a.udk = udk;
+        if(tail == null) {
+            head = a;
+            tail = a;
+        } else {
+            tail.next = a;
+            tail = a;
+        }
+    }
+
+    void printBookList() {
+        BookListElement t = head;
+        while (t != null) {
+            System.out.println(t.udk + " ");
+            t = t.next;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        BooksList book = new BooksList();
+        book.addBack("3234243252");
     }
 }
